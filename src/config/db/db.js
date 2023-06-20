c6664c12f56ea3277d8b6cebb6679b11565a2cb3
Cristian Prochnow <cristianprochnow@gmail.com>
2023-06-19 21:09:36 -0300

    feat: add connection

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -1,11 +1,25 @@
 import { Sequelize } from 'sequelize';
 import connection from './connection.js';
+import Altura from '../../models/Altura.js';
+import Genero from '../../models/Genero.js';
+import Pet from '../../models/Pet.js';
+import Tutor from '../../models/Tutor.js';
+import Porte from '../../models/Porte.js';
 
 const db = {};
 
 db.Sequelize = Sequelize;
 db.connection = connection;
 
+db.altura = Altura;
+db.genero = Genero;
+db.pet = Pet;
+db.tutor = Tutor;
+db.porte = Porte;
 
+db.pet.belongsTo(db.altura, { foreignKey: 'altura' });
+db.pet.belongsTo(db.tutor, { foreignKey: 'tutor' });
+db.pet.belongsTo(db.genero, { foreignKey: 'genero' });
+db.altura.belongsTo(db.porte, { foreignKey: 'porte' });
 
 export default db;
