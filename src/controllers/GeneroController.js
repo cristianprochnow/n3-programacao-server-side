b51626dcdcab6eb41e4c3522dbc283b84954689e
Cristian Prochnow <cristianprochnow@gmail.com>
2023-06-22 18:29:47 -0300

    feat: add routing setup for Genero model

---
+++ src/controllers/GeneroController.js
@@ -0,0 +1,33 @@
+class GeneroController {
+  list(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  show(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  create(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  update(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  delete(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+}
+
+export default GeneroController;
--- src/routes/GeneroRouter.js
+++ src/routes/GeneroRouter.js
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import GeneroController from '../controllers/GeneroController.js';
 
 const GeneroRouter = Router();
+const generoController = new GeneroController();
+
+GeneroRouter.get('/', generoController.list);
+GeneroRouter.get('/:id', generoController.show);
+GeneroRouter.post('/', generoController.create);
+GeneroRouter.put('/:id', generoController.update);
+GeneroRouter.delete('/:id', generoController.delete);
 
 export default GeneroRouter;
