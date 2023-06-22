7e49bf2b464ee4781c09e98e5906be8cd572ce4a
Cristian Prochnow <cristianprochnow@gmail.com>
2023-06-22 18:30:53 -0300

    feat: add routing setup for Pet model

---
+++ src/controllers/PetController.js
@@ -0,0 +1,33 @@
+class PetController {
+  list(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  show(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  create(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  update(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+
+  delete(request, response) {
+    return response.send({
+      success: true
+    });
+  }
+}
+
+export default PetController;
--- src/routes/PetRouter.js
+++ src/routes/PetRouter.js
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import PetController from '../controllers/PetController.js';
 
 const PetRouter = Router();
+const petController = new PetController();
+
+PetRouter.get('/', petController.list);
+PetRouter.get('/:id', petController.show);
+PetRouter.post('/', petController.create);
+PetRouter.put('/:id', petController.update);
+PetRouter.delete('/:id', petController.delete);
 
 export default PetRouter;
