c526c69a0e5849f1087295135834fd6e2f9be8da
Cristian Prochnow <cristianprochnow@gmail.com>
2023-06-22 18:33:13 -0300

    feat: add routing setup for Tutor  model

--- src/routes/TutorRouter.js
+++ src/routes/TutorRouter.js
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import TutorController from '../controllers/TutorController.js';
 
 const TutorRouter = Router();
+const tutorController = new TutorController();
+
+TutorRouter.get('/', tutorController.list);
+TutorRouter.get('/:id', tutorController.show);
+TutorRouter.post('/', tutorController.create);
+TutorRouter.put('/:id', tutorController.update);
+TutorRouter.delete('/:id', tutorController.delete);
 
 export default TutorRouter;
