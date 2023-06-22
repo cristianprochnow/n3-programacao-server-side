828fc5baf696850042c87fa64e0717f64d71cff6
Cristian Prochnow <cristianprochnow@gmail.com>
2023-06-22 18:28:21 -0300

    feat: add routing setup for Altura model

--- src/routes/AlturaRouter.js
+++ src/routes/AlturaRouter.js
@@ -6,5 +6,9 @@ const AlturaRouter = Router();
 const alturaController = new AlturaController();
 
 AlturaRouter.get('/', alturaController.list);
+AlturaRouter.get('/:id', alturaController.show);
+AlturaRouter.post('/', alturaController.create);
+AlturaRouter.put('/:id', alturaController.update);
+AlturaRouter.delete('/:id', alturaController.delete);
 
 export default AlturaRouter;
