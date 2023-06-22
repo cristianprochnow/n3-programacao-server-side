afaa8832a0f8251e4b4f3295d9d7f552bca10b34
Cristian Prochnow <cristianprochnow@gmail.com>
2023-06-22 18:47:43 -0300

    feat: add packages for login

---
+++ src/models/Usuario.js
@@ -0,0 +1,16 @@
+import Sequelize from 'sequelize';
+import connection from '../config/db/connection.js';
+
+const Usuario = connection.define('Usuario', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  apelido: Sequelize.STRING(50),
+  email: Sequelize.STRING(50),
+  nome: Sequelize.STRING(100),
+  password: Sequelize.STRING(48);
+});
+
+export default Usuario;
