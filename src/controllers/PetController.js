7037942bbe68496839639d34ad3314197b186314
Cristian Prochnow <cristianprochnow@gmail.com>
2023-07-01 12:02:08 -0300

    feat: update Pet routes

--- src/controllers/PetController.js
+++ src/controllers/PetController.js
@@ -1,32 +1,179 @@
+import db from '../config/db/db.js';
+
 class PetController {
-  list(request, response) {
-    return response.send({
-      success: true
-    });
+  constructor() {
+    this.pet = db.pet;
   }
 
-  show(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async list(request, response) {
+    let content = { success: false };
+
+    try {
+      const pet = await this.pet.findAll();
+
+      content = {
+        success: true,
+        data: pet
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 
-  create(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async show(request, response) {
+    let content = { success: false };
+
+    try {
+      const { params } = request;
+      const { id } = params;
+
+      if (!id) throw '[id] de filtro não enviado nos parâmetros da rota.';
+
+      const petCadastrado = await this.pet.findByPk(id);
+      if (!petCadastrado) throw `Não existe um registro de Pet com código [${id}].`;
+
+      content = {
+        success: true,
+        result: petCadastrado
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 
-  update(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async create(request, response) {
+    let content = { success: false };
+
+    try {
+      const { body } = request;
+
+      let name = body.nome;
+      let gender = body.genero;
+      let height = body.altura;
+      let guardian = body.tutor;
+
+      const pet = await this.pet.create({
+        nome: name,
+        genero: gender,
+        altura: height,
+        tutor: guardian
+      });
+
+      content = {
+        success: true,
+        result: pet
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 
-  delete(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async update(request, response) {
+    let content = { success: false };
+
+    try {
+      const { body, params } = request;
+      const { id } = params;
+
+      let name = body.nome;
+      let gender = body.genero;
+      let height = body.altura;
+      let guardian = body.tutor;
+
+      if (!id) throw '[id] de filtro não enviado nos parâmetros da rota.';
+
+      const petCadastrado = await this.pet.findByPk(id);
+      if (!petCadastrado) throw `Não existe um registro de pet com código [${id}].`;
+
+      petCadastrado.nome = name;
+      petCadastrado.genero = gender;
+      petCadastrado.altura = height;
+      petCadastrado.tutor = guardian;
+
+      const pet = await petCadastrado.save();
+
+      content = {
+        success: true,
+        result: pet
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
+  }
+
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async delete(request, response) {
+    let content = { success: false };
+
+    try {
+      const { params } = request;
+      const { id } = params;
+
+      if (!id) throw '[id] de filtro não enviado nos parâmetros da rota.';
+
+      const petCadastrado = await this.pet.findByPk(id);
+      if (!petCadastrado) throw `Não existe um registro de Pet com código [${id}].`;
+
+      await petCadastrado.destroy();
+
+      content = { success: true };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 }
 
--- src/routes/PetRouter.js
+++ src/routes/PetRouter.js
@@ -4,10 +4,10 @@ import PetController from '../controllers/PetController.js';
 const PetRouter = Router();
 const petController = new PetController();
 
-PetRouter.get('/', petController.list);
-PetRouter.get('/:id', petController.show);
-PetRouter.post('/', petController.create);
-PetRouter.put('/:id', petController.update);
-PetRouter.delete('/:id', petController.delete);
+PetRouter.get('/', petController.list.bind(petController));
+PetRouter.get('/:id', petController.show.bind(petController));
+PetRouter.post('/', petController.create.bind(petController));
+PetRouter.put('/:id', petController.update.bind(petController));
+PetRouter.delete('/:id', petController.delete.bind(petController));
 
 export default PetRouter;
