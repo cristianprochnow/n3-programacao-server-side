a1d8ebd1ebf7e8dc7cf95e9895cfb7376b5392c5
Cristian Prochnow <cristianprochnow@gmail.com>
2023-07-01 11:47:16 -0300

    feat: finish Genero route

--- src/controllers/GeneroController.js
+++ src/controllers/GeneroController.js
@@ -1,32 +1,166 @@
+import db from '../config/db/db.js';
+
 class GeneroController {
-  list(request, response) {
-    return response.send({
-      success: true
-    });
+  constructor() {
+    this.genero = db.genero;
+  }
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async list(request, response) {
+    let content = { success: false };
+
+    try {
+      const genero = await this.genero.findAll();
+
+      content = {
+        success: true,
+        data: genero
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 
-  show(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async show(request, response) {
+    let content = { success: false };
+
+    try {
+      const { params } = request;
+      const { id } = params;
+
+      if (!id) throw '[id] de filtro não enviado nos parâmetros da rota.';
+
+      const generoCadastrado = await this.genero.findByPk(id);
+      if (!generoCadastrado) throw `Não existe um registro de Gênero com código [${id}].`;
+
+      content = {
+        success: true,
+        result: generoCadastrado
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 
-  create(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async create(request, response) {
+    let content = { success: false };
+
+    try {
+      const { body } = request;
+
+      let description = body.descricao;
+
+      const genero = await this.genero.create({
+        descricao: description
+      });
+
+      content = {
+        success: true,
+        result: genero
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 
-  update(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async update(request, response) {
+    let content = { success: false };
+
+    try {
+      const { body, params } = request;
+      const { id } = params;
+
+      let description = body.descricao;
+
+      if (!id) throw '[id] de filtro não enviado nos parâmetros da rota.';
+
+      const generoCadastrado = await this.genero.findByPk(id);
+      if (!generoCadastrado) throw `Não existe um registro de Gênero com código [${id}].`;
+
+      generoCadastrado.descricao = description;
+
+      const genero = await generoCadastrado.save();
+
+      content = {
+        success: true,
+        result: genero
+      };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 
-  delete(request, response) {
-    return response.send({
-      success: true
-    });
+  /**
+   * @param {import('express').Request} request
+   * @param {import('express').Response} response
+   */
+  async delete(request, response) {
+    let content = { success: false };
+
+    try {
+      const { params } = request;
+      const { id } = params;
+
+      if (!id) throw '[id] de filtro não enviado nos parâmetros da rota.';
+
+      const generoCadastrado = await this.genero.findByPk(id);
+      if (!generoCadastrado) throw `Não existe um registro de Gênero com código [${id}].`;
+
+      await generoCadastrado.destroy();
+
+      content = { success: true };
+    } catch (error) {
+      console.error(error);
+
+      content = {
+        success: false,
+        error
+      };
+    } finally {
+      return response.send(content);
+    }
   }
 }
 
--- src/routes/GeneroRouter.js
+++ src/routes/GeneroRouter.js
@@ -4,10 +4,10 @@ import GeneroController from '../controllers/GeneroController.js';
 const GeneroRouter = Router();
 const generoController = new GeneroController();
 
-GeneroRouter.get('/', generoController.list);
-GeneroRouter.get('/:id', generoController.show);
-GeneroRouter.post('/', generoController.create);
-GeneroRouter.put('/:id', generoController.update);
-GeneroRouter.delete('/:id', generoController.delete);
+GeneroRouter.get('/', generoController.list.bind(generoController));
+GeneroRouter.get('/:id', generoController.show.bind(generoController));
+GeneroRouter.post('/', generoController.create.bind(generoController));
+GeneroRouter.put('/:id', generoController.update.bind(generoController));
+GeneroRouter.delete('/:id', generoController.delete.bind(generoController));
 
 export default GeneroRouter;
